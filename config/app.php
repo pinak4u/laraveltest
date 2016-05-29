bf8da41a324e2fbb14c5833476613b45f4f2183f
Pinak Patel <pinak007@gmail.com>
2016-05-29 22:51:59 +0530

    Debugbar, IdeHelper, added
    Debugbard and IdeHelper are added to the main project.

--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Third Party Service Provider
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -201,6 +204,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //Third Party Facades
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
